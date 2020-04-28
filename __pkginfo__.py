f42b0280b21685d10a8c719d9afc38f50dde0d10
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-28 22:35:15 +0100

    Changed tests to use tox

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -15,71 +15,68 @@
 
 # This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
 
-import os.path
+import pathlib
 
-
-copyright   = """
-2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+copyright = """
+2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
 VERSION = "0.1.10"
 
-modname            = "mathematical"
-py_modules		   = None
-entry_points	   = None
-#py_modules         = [modname]
-#entry_points       = {
+modname = "mathematical"
+py_modules = None
+entry_points = None
+# py_modules         = [modname]
+# entry_points       = {
 #	'console_scripts': [
 #		'package_name=package_name:main',
 #	]}
 
-license            = 'LGPLv3+'
+license = 'LGPLv3+'
 
-short_desc         = 'Mathematical tools for python'
+short_desc = 'Mathematical tools for python'
 
-author             = "Dominic Davis-Foster"
-author_email       = "dominic@davis-foster.co.uk"
-github_username	   = "domdfcoding"
-web                = github_url = f"https://github.com/{github_username}/{modname}"
-
-
-def get_srcdir():
-	filename = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
-	return os.path.realpath(filename)
-
-
-srcdir = get_srcdir()
-
-
-def read(*rnames):
-	return open(os.path.join(srcdir, *rnames)).read()
-
+author = "Dominic Davis-Foster"
+author_email = "dominic@davis-foster.co.uk"
+github_username = "domdfcoding"
+web = github_url = f"https://github.com/{github_username}/{modname}"
+project_urls = {
+		"Documentation": f"https://{modname}.readthedocs.io",  # TODO: Make this link match the package version
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
 
 # Get info from files; set: long_description
-long_description = (read("README.rst") + '\n')
+if pathlib.Path.cwd().name == "doc-source":
+	print(pathlib.Path.cwd().parent / "README.rst")
+	install_requires = (pathlib.Path.cwd().parent / "requirements.txt").read_text().split("\n")
+	long_description = (pathlib.Path.cwd().parent / "README.rst").read_text() + '\n'
+else:
+	print(pathlib.Path("README.rst"))
+	install_requires = pathlib.Path("requirements.txt").read_text().split("\n")
+	long_description = pathlib.Path("README.rst").read_text() + '\n'
 
-install_requires   = read("requirements.txt").split("\n")
 
 classifiers = [
-	"Development Status :: 4 - Beta",
-	# "Development Status :: 5 - Production/Stable",
-	# "Development Status :: 6 - Mature",
-	# "Development Status :: 7 - Inactive",
-	
-	"Operating System :: OS Independent",
-	
-	"Intended Audience :: Developers",
-	"Intended Audience :: Education",
-	"Intended Audience :: End Users/Desktop",
-	"Intended Audience :: Science/Research",
-	
-	"License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-	
-	"Programming Language :: Python :: 3.6",
-	"Programming Language :: Python :: 3.7",
-	"Programming Language :: Python :: 3.8",
-	"Programming Language :: Python :: 3 :: Only",
-	"Programming Language :: Python :: Implementation :: CPython",
-	
-	"Topic :: Software Development :: Libraries :: Python Modules",
-]
+		"Development Status :: 4 - Beta",
+		# "Development Status :: 5 - Production/Stable",
+		# "Development Status :: 6 - Mature",
+		# "Development Status :: 7 - Inactive",
+		
+		"Operating System :: OS Independent",
+		
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"Intended Audience :: End Users/Desktop",
+		"Intended Audience :: Science/Research",
+		
+		"License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+		
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: Implementation :: CPython",
+		
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		]
--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 """Setup script"""
 
-from __pkginfo__ import \
-    author,           author_email,       install_requires,          \
-    license,          long_description,   classifiers,               \
-    entry_points,     modname,            py_modules,                \
-    short_desc,       VERSION,            web
+from __pkginfo__ import (
+	author, author_email, install_requires,
+	license, long_description, classifiers,
+	entry_points, modname, py_modules,
+	short_desc, VERSION, web, project_urls,
+	)
 
 from setuptools import setup, find_packages
+
 setup(
-       author             = author,
-       author_email       = author_email,
-       classifiers        = classifiers,
-       description        = short_desc,
-       entry_points       = entry_points,
-       install_requires   = install_requires,
-       license            = license,
-       long_description   = long_description,
-       name               = modname,
-       packages           = find_packages(exclude=("tests",)),
-       py_modules         = py_modules,
-       url                = web,
-       version            = VERSION)
+		author=author,
+		author_email=author_email,
+		classifiers=classifiers,
+		description=short_desc,
+		entry_points=entry_points,
+		install_requires=install_requires,
+		license=license,
+		long_description=long_description,
+		name=modname,
+		packages=find_packages(exclude=("tests",)),
+		py_modules=py_modules,
+		url=web,
+		project_urls=project_urls,
+		version=VERSION,
+		python_requires=">=3.6",
+		include_package_data=True,
+		)
