fdebbb7395be0e9232fc3229ff128c17e99a5fbb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-11 08:59:44 +0100

    Updated files with `git_helper`

--- README.rst
+++ README.rst
@@ -2,9 +2,11 @@
 mathematical
 **********************
 
-.. image:: https://travis-ci.com/domdfcoding/mathematical.svg?branch=master
+.. start shields
+
+.. image:: https://img.shields.io/travis/com/domdfcoding/mathematical/master?logo=travis
     :target: https://travis-ci.com/domdfcoding/mathematical
-    :alt: Build Status
+    :alt: Travis Build Status
 .. image:: https://readthedocs.org/projects/mathematical/badge/?version=latest
     :target: https://mathematical.readthedocs.io/en/latest/?badge=latest
     :alt: Documentation Status
@@ -14,10 +16,19 @@ mathematical
 .. image:: https://img.shields.io/pypi/pyversions/mathematical.svg
     :target: https://pypi.org/project/mathematical/
     :alt: PyPI - Python Version
+.. image:: https://img.shields.io/conda/v/domdfcoding/mathematical
+    :alt: Conda
+    :target: https://anaconda.org/domdfcoding/mathematical
 .. image:: https://coveralls.io/repos/github/domdfcoding/mathematical/badge.svg?branch=master
     :target: https://coveralls.io/github/domdfcoding/mathematical?branch=master
     :alt: Coverage
+.. image:: https://img.shields.io/badge/License-LGPL%20v3-blue.svg
+    :alt: License
+    :target: https://github.com/domdfcoding/mathematical/blob/master/LICENSE
+.. image:: https://img.shields.io/github/languages/top/domdfcoding/mathematical
+    :alt: GitHub top language
 
+.. end shields
 
 Mathematical tools for python
 
--- lint_roller.sh
+++ lint_roller.sh
@@ -1,35 +1,35 @@
 # fix these
 declare -a errors=(
   E301 E302 E303 E304 E305 E306 E502 E265
-  W291 W292 W293 W391 E226 E225
+  W291 W292 W293 W391 E226 E225 E241
   )
 
 # Only warn for these
 declare -a warnings=(
-  E101 E111 E112 E113 E121 E122 E123 E124 E125
+  E101 E111 E112 E113 E121 E122 E124 E125
   E127 E128 E129 E131 E133 E201 E202 E203
   E211 E222 E223 E224 E225 E227 E228 E231
-  E241 E242 E251 E261 E262 E271 E272 E402
-  E703 E711 E712 E713 E714 E721 E722 W503 W504
+  E242 E251 E261 E262 E271 E272 E402
+  E703 E711 E712 E713 E714 E721 W503 W504
   )
 
-if [ -z "$(git status --porcelain --untracked-files=no)" ] || [ $1 == "-f" ]; then
+if [ -z "$(git status --porcelain --untracked-files=no)" ] || [ "$1" == "-f" ]; then
   # Working directory clean
 
   for error in "${errors[@]}"
   do
     echo "Correcting $error"
     autopep8 --in-place --select "$error" -a --recursive mathematical/
-    flake8 --select "$error" mathematical/
+    >&2 flake8 --select "$error" mathematical/
     autopep8 --in-place --select "$error" -a --recursive tests/
-    flake8 --select "$error" tests/
+    >&2 flake8 --select "$error" tests/
 
   done
 
   for warning in "${warnings[@]}"; do
     echo "Searching for $warning"
-    flake8 --select "$warning" mathematical/
-    flake8 --select "$warning" tests/
+    >&2 flake8 --select "$warning" mathematical/
+    >&2 flake8 --select "$warning" tests/
   done
 
   exit 0
