82e1be06b34aceebe6dee5a122a17a749adb029b
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-15 20:32:07 +0000

    Updated files with 'repo_helper'.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -19,8 +19,6 @@ __all__ = [
 		"__version__",
 		"modname",
 		"pypi_name",
-		"py_modules",
-		"entry_points",
 		"__license__",
 		"__author__",
 		"short_desc",
@@ -29,13 +27,11 @@ __all__ = [
 		"github_username",
 		"web",
 		"github_url",
-		"project_urls",
 		"repo_root",
-		"long_description",
 		"install_requires",
 		"extras_require",
-		"classifiers",
-		"keywords",
+		"project_urls",
+
 		"import_name",
 		]
 
@@ -44,59 +40,30 @@ __copyright__ = """
 """
 
 __version__ = "0.1.12"
-
 modname = "mathematical"
 pypi_name = "mathematical"
 import_name = "mathematical"
-py_modules = []
-entry_points = {
-		"console_scripts": [],
-		}
-
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-
 short_desc = "Mathematical tools for Python 📐 🐍 🛠️"
-
 __author__ = author = "Dominic Davis-Foster"
 author_email = "dominic@davis-foster.co.uk"
 github_username = "domdfcoding"
-web = github_url = f"https://github.com/domdfcoding/mathematical"
-project_urls = {
-		"Documentation": f"https://mathematical.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}
-
+web = github_url = "https://github.com/domdfcoding/mathematical"
 repo_root = pathlib.Path(__file__).parent
+install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
+extras_require = {'all': []}
 
-# Get info from files; set: long_description
-long_description = (repo_root / "README.rst").read_text(encoding="utf-8").replace("0.1.12", __version__) + '\n'
+
+
 conda_description = """Mathematical tools for Python
 
 
 Before installing please ensure you have added the following channels: domdfcoding, conda-forge"""
 __all__.append("conda_description")
 
-install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
-extras_require = {'all': []}
 
-classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Education',
-		'Intended Audience :: End Users/Desktop',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 3 :: Only',
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: Python :: 3.7',
-		'Programming Language :: Python :: 3.8',
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Typing :: Typed',
-
-		]
-
-keywords = "mathematics utilities"
+project_urls = {
+		"Documentation": "https://mathematical.readthedocs.io",
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
