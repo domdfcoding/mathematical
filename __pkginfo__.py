5166ad858c77e2a07ebb276259ad741f3d7d5aba
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-17 20:29:17 +0100

    Bump version: 0.1.12 → 0.1.13

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2019-2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 modname = "mathematical"
 pypi_name = "mathematical"
 import_name = "mathematical"
